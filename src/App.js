ac522ec9191ee946651fb0d8aaef0b0de3eac2b2
jerome.rayos <jerome.rayos@maroonstudios.com>
2023-09-14 09:49:53 +0800

    Feat: initial login with google

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 function App() {
+  const login = useGoogleLogin({
+    onSuccess: response => {
+      console.log(response)
+      const { code } = response;
+      axios
+        .post('/webapi/create-token', { code })
+        .then(response => {
+          console.log('from-BE')
+          console.log(response)
+          console.log({
+            this_is_what_you_need: response.data.refresh_token
+          })
+          // get the response.data.refresh_token
+        })
+        .catch(error => {
+          console.log(error.message)
+        })
+    },
+    onError: response => {
+      console.log(response)
+    },
+    flow: 'auth-code',
+    scope: 'openid email profile https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/drive'
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <button
+          onClick={() => login()}
         >
-          Learn React
-        </a>
-      </header>
+          Sign in with Google
+        </button>
+      </div>
     </div>
   );
 }
